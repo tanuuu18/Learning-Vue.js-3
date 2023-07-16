ca451ccbb8398a0fc546198c24f5d911b5f993ba
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-16 20:11:02 +0530

    10. v-bind shorthand

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   
-  <h2 v-bind:id="headingId">Heading</h2>
-  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2  :id="headingId">Heading</h2>
+  <button  :disabled="isDisabled">Bind</button>
   <h2 class="underline">Underlined text</h2>
-  <h2 class="underline"  v-bind:class="status">Status</h2>
-  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 class="underline"   :class="status">Status</h2>
+  <h2  :class="isPromoted && 'promoted'">Promoted Movie</h2>
   <!-- Conditional Binding of class -->
-  <h2 v-bind:class="isSoldout ? 'sold-out':'new'">Soldout ? movie</h2>
+  <h2  :class="isSoldout ? 'sold-out':'new'">Soldout ? movie</h2>
   <!-- Array binding of class -->
-  <h2 v-bind:class="['new','promoted']">Newly promoted movie</h2>
-  <h2 v-bind:class="[isPromoted && 'promoted',isSoldout ? 'sold-out':'new']">Array conditional movie</h2>
+  <h2  :class="['new','promoted']">Newly promoted movie</h2>
+  <h2  :class="[isPromoted && 'promoted',isSoldout ? 'sold-out':'new']">Array conditional movie</h2>
   <!-- Object binding of class -->
-  <h2 v-bind:class="{
+  <h2  :class="{
     promoted: isPromoted,
     new: !isSoldout,
     'sold-out':isSoldout
@@ -20,17 +20,17 @@
   <!-- Object syntax -->
  <!-- font-size is not a single word so, append it with ''.
  instead of this we can use camel-case i.e. fontSize -->
-  <h2 v-bind:style="{
+  <h2  :style="{
     color:highlightColor,
     fontSize:headerSize+'px',
     padding:'20px'
   }">Inline Style</h2>
 
-  <h2 v-bind:style="headerStyleObject">Style Object</h2>
+  <h2  :style="headerStyleObject">Style Object</h2>
   <!-- Array syntax -->
   <!-- override the previous style object if same property changes -->
-  <div v-bind:style="[baseStyleObject,successStyleObject]">Success Style</div>
-  <div v-bind:style="[baseStyleObject,dangerStyleObject]">Danger Style</div>
+  <div  :style="[baseStyleObject,successStyleObject]">Success Style</div>
+  <div  :style="[baseStyleObject,dangerStyleObject]">Danger Style</div>
   
 </template>
 
