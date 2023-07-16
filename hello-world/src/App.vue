85ca52d313dd5fa632f06a054ab95c86446e6c23
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-16 18:41:25 +0530

    7. Binding to Attributes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,7 @@
   <div v-html="learning"></div>
   <div v-html="hack"></div>
   <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
 </template>
 
 <script>
@@ -18,7 +19,8 @@ export default {
       name:"Tanushree",
       learning:"<b>Vue Js</b>",
       hack:`<a href="#" onclick="alert('You have been hacked!')">Win a Prize</a>`,
-      headingId:"heading"
+      headingId:"heading",
+      isDisabled:true
     }
   }
 }
