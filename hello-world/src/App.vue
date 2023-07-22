871e73d6f8367479de195700dc91c01b1a80b166
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-22 08:10:25 +0000

    14. Lists and Keys

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <!-- Array of Strings -->
-  <h2 v-for="name in names" :key="name">{{ name }}</h2>
-  <!-- with index -->
-  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-
-  <!-- Array of Objects -->
-  <h2 v-for="name in fullNames" :key="name.first">
-    {{ name.first }} {{ name.last }}
-  </h2>
-
-  <!-- Array of Arrays -->
-  <div v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name }}</h2>
-    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-  </div>
-  <!-- Object of key-value pairs -->
-  <h2 v-for="(value, key, index) in myInfo" :key="value">
-    {{ index }} {{ key }} {{ value }}
-  </h2>
-  <!-- Block of HTML elements  -> use template tag -->
-  <template v-for="name in names" :key="name">
+   <template v-for="name in names" :key="name">
     <h2>{{ name }}</h2>
+    <input placeholder="Last name" />
     <hr />
   </template>
+  <button @click="shuffle">Shuffle!</button>
   
 </template>
 
@@ -30,31 +12,18 @@
 
 
 export default {
+  import _ from "lodash";
   name: 'App',
   data(){
     return {
     names: ["Tanu", "Sumit", "Khushi"],
-      fullNames: [
-        { first: "Bruce", last: "Wayne" },
-        { first: "Clark", last: "Kent" },
-        { first: "Princess", last: "Diana" },
-      ],
-      actors: [
-        {
-          name: "Christian Bale",
-          movies: ["Batman", "The Prestige"],
-        },
-        {
-          name: "Di Caprio",
-          movies: ["Titanic", "Inception"],
-        },
-      ],
-      myInfo: {
-        name: "Tanu",
-        profession: "Software Developer",
-        learning: "Vue Js",
-      },
     };
+  },
+  methods: {
+    shuffle() {
+      console.log(this.names);
+      this.names = _.shuffle(this.names);
+    },
   },
 };
 </script>
