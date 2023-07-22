ab72d33c6ee51060a596c57e5bc20568a6b68955
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-22 21:50:22 +0530

    20. Form Handling (Part-2)

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,7 +5,7 @@
     </pre>
   </div>
   <!-- Job search form -->
-  <form action="">
+  <form @submit="submitForm">
     <!-- Step 2: Add HTML tag -->
     <!-- Step 3:bind the data properties using v-model directives -->
     <div>
@@ -38,6 +38,88 @@
         <option value="singapore">Singapore</option>
       </select>
     </div>
+     <!-- Single check box -->
+    <div>
+      <!-- if you want true/false -->
+      <!-- <input
+        type="checkbox"
+        id="remoteWork"
+        v-model="formValues.remoteWork"
+      /> -->
+      <!-- if you wany yes / no -->
+      <input
+        type="checkbox"
+        id="remoteWork"
+        v-model="formValues.remoteWork"
+        true-value="yes"
+        false-value="no"
+      />
+      <label for="remoteWork">Open to remote work ?</label>
+    </div>
+
+    <!-- Multiple Checkboxes -->
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        v-model="formValues.skillSet"
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+
+    <!-- Radio group -->
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="6-10">6-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="10+">10+</label>
+    </div>
+
+    <!-- Submit button -->
+    <div>
+      <button>Submit</button>
+    </div>
   </form>
 </template>
 
@@ -48,17 +130,39 @@ export default {
   name: 'App',
   data(){
     return {
-        // Step: 1  create data properties
+       // Step: 1  create data properties
       formValues: {
         name: "",
+
         profileSummary: "",
+
+        //Single select dropdown
         country: "",
+
+        // multi-select dropdown
         jobLocation: [],
+
+        //Single checkbox
+        //if you want true/false
+        //remoteWork: "false",
+
+        //if you wany yes / no
+        remoteWork: "no",
+
+        //Multiple checkbox
+        skillSet: [],
+
+        //Radio group
+        yearsOfExperience: "",
+      
       },
     };
   },
    methods: {
-    
+     submitForm(event) {
+      event.preventDefault();
+      console.log("Form values: ", this.formValues);
+    },
   },
 };
 </script>
