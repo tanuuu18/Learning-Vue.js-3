a029f8bf502ba69f22c1d748698749812e7acd25
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-29 17:44:28 +0530

    27. Watchers

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,31 +1,11 @@
 <template>
- 
-<!-- composed new data properties from existing properties -->
- <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
-  <h2>Computed fullname: {{ fullName }}</h2>
- 
-  <button @click="changeFullName">Change full name</button>
- 
-<button @click="items.push({ id: 4, title: 'Keyboard', price: 50 })">
-    Add Item
-  </button>
-   <!-- can be used at multiple places at multiple times -->
- <!-- Computed properties automatically recalculated when the dependency have changed -->
- <!-- Both(Computed and method) will return same result.Buth difference is that
-  Computed properties are cached, this increase App performance -->
-  <h2>Computed Total : {{ total }}</h2>
+   <h2>Volume Tracker (0-20)</h2>
+  <h2>Current Volume - {{ volume }}</h2>
 
-  <h2>Method Total - {{ getTotal() }}</h2>
-  <!-- as we type in input, only getTotal method is recalculated not computed -->
-  <input type="text" v-model="country" />
-
- <template v-for="item in items" :key="item.id">
-    <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
-  </template>
-  <!-- Computed properties are cached, this increase App performance  -->
-  <h2 v-for="item in expensiveItems" :key="item.id">
-    {{ item.title }} {{ item.price }}
-  </h2>
+  <div>
+    <button @click="volume += 2">Increase</button>
+    <button @click="volume -= 2">Decrease</button>
+  </div>
 </template>
 
 <script>
@@ -35,60 +15,18 @@ export default {
   name: 'App',
   data(){
     return {
-       firstName: "Tanu",
-      lastName: "Shree",
-     items: [
-        {
-          id: 1,
-          title: "TV",
-          price: 100,
-        },
-        {
-          id: 2,
-          title: "Phone",
-          price: 200,
-        },
-        {
-          id: 3,
-          title: "Laptop",
-          price: 300,
-        },
-      ],
-      country: "",
-    };
-  },
-   methods: {
-    getTotal() {
-      console.log("getTotal method");
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
+      volume: 0,
     },
-     changeFullName() {
-      this.fullName = "Web Development";
-    },
-   },
-  computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      },
-      set(value) {
-        const names = value.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[1];
-      },
-    },
-    total() {
-      console.log("total computed property");
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
-    },
-   expensiveItems() {
-      return this.items.filter((item) => item.price > 100);
+   methods: { },
+  computed: { },
+ watch: {
+    volume(newValue, oldValue) {
+     //Will show alert on 16 while increaseing volume not on decresing
+      if (newValue > oldValue && newValue === 16) {
+        alert(
+          "Listening to a high volume for a long time may damage your hearing"
+        );
+      }
     },
   },
 };
