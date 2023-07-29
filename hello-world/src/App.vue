c5f963567f8c0a82c803ebe103e4279559f06f1b
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-29 13:14:10 +0000

    29. Components

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,63 +1,16 @@
 <template>
-   <h2>Volume Tracker (0-20)</h2>
-  <h2>Current Volume - {{ volume }}</h2>
-
-  <div>
-    <button @click="volume += 2">Increase</button>
-    <button @click="volume -= 2">Decrease</button>
-  </div>
-   <input type="text" v-model="movie" />
-    <input type="text" v-model="movieInfo.title" />
-  <input type="text" v-model="movieInfo.actor" />
-  <div><button @click="movieList.push('Wonder women')">Add movie</button></div>
+    <Greet />
 </template>
 
 <script>
-
+import Greet from "./components/Greet.vue";
 
 export default {
   name: 'App',
-  data(){
-    return {
-      volume: 0,
-       //After setting this value the api will not call
-      movie: "Tanu",
-       movieInfo: {
-        title: "",
-        actor: "",
-      },
-      movieList: ["Batman", "Superman"],
-    },
-   methods: { },
-  computed: { },
-   //Watchers executed only when the data has changed
- watch: {
-    volume(newValue, oldValue) {
-     //Will show alert on 16 while increaseing volume not on decresing
-      if (newValue > oldValue && newValue === 16) {
-        alert(
-          "Listening to a high volume for a long time may damage your hearing"
-        );
-      }
-    },
-    movie(newValue) {
-      console.log(`Calling an API with movie name = ${newValue}`);
-    },
-    movieInfo: {
-      handler(newValue) {
-        console.log(
-          `Calling an API with movie title = ${newValue.title} and actor = ${newValue.actor}`
-        );
-      },
-      deep: true,
-    },
-    movieList: {
-      handler(newValue) {
-        console.log(`Updated list ${newValue}`);
-      },
-      deep: true,
-    },
+  components: {
+    Greet: Greet,
   },
+       
 };
 </script>
 
@@ -66,7 +19,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ hello-world/src/components/Greet.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Hello Tanu!!</h2>
+</template>
+<script>
+export default {
+  name: "Greet",
+};
+</script>
